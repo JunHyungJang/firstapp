a14b1ca5c3fe8df0a5bfeb07ce744faa01e2b10c
JunhyungJang <jsy3535@dgist.ac.kr>
2022-03-13 15:18:40 +0900

    20220313

--- firstproject/App.js
+++ firstproject/App.js
@@ -1,31 +1,49 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Header from './Header'
-import Body from './Body'
+import {SafeAreaView, StyleSheet, TextInput, View, Text, ScrollView} from 'react-native';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Todo App</Text>
-        <Header />
-        <Body />
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>Hello Todolist</Text>
+      <View style={styles.card}>
+        <TodoInsert/>
+        <TodoList/>
       </View>
-    );
-  }
-}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    paddingTop: 50,
-    backgroundColor: "#EEE",
+    backgroundColor: '#3143e8',
   },
-  title: {
-    fontWeight: "800",
-    fontSize: 30, 
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    backgroundColor: '#3143e8',
+  },
+  card: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 10, // to provide rounded corners
+    borderTopRightRadius: 10, // to provide rounded corners
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
     marginLeft: 20,
-    marginBottom: 20,
-  }
+  },
 });
+
+export default App;
---
+++ firstproject/components/TodoListItem.js
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import {Button, StyleSheet, TextInput, View} from 'react-native'
+
+// const TodoInsert
+
+
+// const styles = StyleSheet.create({
+//     inputcontainer : {
+//         flexDirection : 'row',
+//         justifyContent : 'space-between',
+//         alignItems: 'center'
+//     },
+//     input : {
+//         flex : 1,
+//         padding 20,
+//         borderBottomColor: '#bbb'
+//         borderBottomWidth: 1,
+//         fontSize : 24,
+//         marginLeft : 20,
+//     },
+
+//     button : {
+//         marginRight : 10,
+//     },
+// });
+
+// export default TodoInsert
