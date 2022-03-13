6c1b979f906ce935d768565e47e40abbe0f4a750
JunhyungJang <jsy3535@dgist.ac.kr>
2022-03-13 21:20:30 +0900

    20220303

--- firstproject/components/TodoListItem.js
+++ firstproject/components/TodoListItem.js
@@ -4,27 +4,30 @@ import Icon from 'react-native-vector-icons/AntDesign'
 
 
 
-const TodoListItem = () => {
+const TodoListItem = ({textValue, id, checked, onRemove, onToggle}) => {
     return (
-    <View style = {styles.container}>
-        <TouchableOpacity>
-            <View style = {styles.completeCircle}>
-            <Icon name = "circledowno" size={30} color= "#3143e8"/>
+        <View style={styles.container}>
+        <TouchableOpacity onPressOut={onToggle(id)}>
+          {checked ? (
+            <View style={styles.completeCircle}>
+              <Icon name="circledowno" size={30} color="#3143e8" />
             </View>
-            
+          ) : (
+            <View style={styles.circle} />
+          )}
         </TouchableOpacity>
-        <Text style = {[styles.text, styles.strikeText]} >
-            Items will be shown here
-        
-        </Text>
-        <TouchableOpacity style={styles.buttonContainer}>
-            <Text style={styles.buttonText}>
-            <Icon name="delete" size={30} color="#e33057" />
+        <Text
+            style={[styles.text,checked ? styles.strikeText : styles.unstrikeText,]}>
+            {textValue}
             </Text>
-        </TouchableOpacity>
-    </View>
-    )
-}
+            <TouchableOpacity style={styles.buttonContainer}>
+                <Text style={styles.buttonText} onPress={onRemove(id)}>
+                    <Icon name="delete" size={30} color="#e33057" />
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
 
 
 
